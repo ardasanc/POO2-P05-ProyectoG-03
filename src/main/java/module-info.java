504b3269a7a4ba2003f4f8966e63f36239d57bbd
Ariana Sánchez <ardasanc@espol.edu.ec>
2021-12-23 13:52:04 -0500

    Controllers de Mascota

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.test1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.test1 to javafx.fxml;
     exports com.mycompany.test1;
