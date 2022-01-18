e12e41d3d72f71ef851619b0c37a6d98e59f5324
Milena-Rodriguez <mildarod@espol.edu.ec>
2022-01-18 11:46:43 -0500

    Ciudades

---
+++ src/main/java/com/mycompany/test1/AdministrarCiudad.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.test1;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author PC
+ */
+public class AdministrarCiudad {
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+    @FXML
+    private void switchToAgregarCiudad() throws IOException{
+        App.setRoot("AgregarCiudades");
+    }
+}
